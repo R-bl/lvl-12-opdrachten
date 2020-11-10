5cb209059a6799bffb8e155dde2dd9269c5e797e
Rbloemhard <56073780+Rbloemhard@users.noreply.github.com>
2020-11-10 21:03:46 +0100

    Add files via upload

---
+++ my-first-javascript/script.js
@@ -0,0 +1,14 @@
+let name = 'Raymond';
+console.log(name);
+
+let x = 4;
+console.log(x + x);
+let y = '4';
+console.log(y + y);
+x * x;
+x % x;
+x - x;
+let age = 30;
+typeof(30);
+age = '30';
+typeof('30');
