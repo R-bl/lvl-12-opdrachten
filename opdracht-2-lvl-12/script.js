84ed219cd1b02ab22d3445b0456dd5288fbd7728
Rbloemhard <56073780+Rbloemhard@users.noreply.github.com>
2020-11-12 14:09:49 +0100

    Add files via upload

---
+++ opdracht-2-lvl-12/script.js
@@ -0,0 +1,5 @@
+console.log("Hallo Winc Academy studenten");
+/*Dit is een Grote som Comments worden niet weergegeven, het wordt genegeerd. Vandaar een comment.
+console.log(1230941 * 1823794);
+
+console.log(637263 / 54); */
